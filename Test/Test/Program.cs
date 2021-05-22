827cc872e773b7c45afd83c1087442299e3c04ff
gao <976292406@qq.com>
2021-05-22 23:51:37 +0800

    helloworld

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -6,7 +6,7 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!123");
+            Console.WriteLine("Hello World!");
         }
     }
 }
