aaf524152431e48eec9b503621cc20e01765f855
ErhuoEECS <841254134@qq.com>
2021-05-28 16:01:23 +0800

    注册和主页

--- CourseInfoSharingPlatform/CourseInfoSharingPlatform/Views/StuMainPage.xaml.cs
+++ CourseInfoSharingPlatform/CourseInfoSharingPlatform/Views/StuMainPage.xaml.cs
@@ -10,20 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
-using CourseInfoSharingPlatform.Views;
-namespace CourseInfoSharingPlatform
+
+namespace CourseInfoSharingPlatform.Views
 {
     /// <summary>
-    /// MainWindow.xaml 的交互逻辑
+    /// StuMainPage.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StuMainPage : Window
     {
-        public MainWindow()
+        public StuMainPage()
         {
             InitializeComponent();
-            new StuMainPage().Show();
         }
     }
 }
