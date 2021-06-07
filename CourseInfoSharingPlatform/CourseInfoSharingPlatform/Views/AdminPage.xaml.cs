121585c6770044ed6fa60fe0ded5e13d44086dbb
peter2001666 <2306200297@qq.com>
2021-06-07 22:08:25 +0800

    nihao

---
+++ CourseInfoSharingPlatform/CourseInfoSharingPlatform/Views/AdminPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CourseInfoSharingPlatform.Views
+{
+    /// <summary>
+    /// AdminPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class AdminPage : Window
+    {
+        public AdminPage()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
